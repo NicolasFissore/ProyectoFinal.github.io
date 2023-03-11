0a0c85fc91c651223dccc399b425069d509c3a6d
NicolasFissore <124084013+NicolasFissore@users.noreply.github.com>
2023-03-11 12:54:06 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,4 @@
+document.getElementById("boton").onclick = function () {
+    console.log("Capturamos el evento click")
+    document.getElementById("demo").innerHTML = "Tel: 2657-648512 E-mail: nmfissore98@gmail.com"
+}
